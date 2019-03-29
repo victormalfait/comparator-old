187d5a6503822eb05acfdd0a0948f12cac770bb7
Malfait Victor <victor.malfait@gmail.com>
2019-03-29 17:10:17 +0100

    add reactJS front

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import { Dashboard } from './components/Dashboard/Dashboard.js';
+import { Home } from './components/Home/Home.js';
+import { Signup } from './components/Signup/Signup.js';
+import { Header } from './components/Shared/Header.js';
+import { PrivateRoute } from './components/PrivateRoute.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Header/>
+          <div className="App-content">
+              <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route exact path ="/signup" component={Signup}/>
+                  <PrivateRoute path='/dashboard' component={Dashboard} />
+              </Switch>
+          </div>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/front/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Home extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <div>Test</div>
+    );
+  }
+}
